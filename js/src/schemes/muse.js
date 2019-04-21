5b69deefb95b2827251c9b74907a0c72fcc4a500
sxd666 <1377615418@qq.com>
2019-04-21 20:54:57 +0800

    Site updated: 2019-04-21 20:54:55

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 22 2019 16:15:29 GMT+0800 (GMT+08:00)
+// build time:Sun Apr 21 2019 20:45:34 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
